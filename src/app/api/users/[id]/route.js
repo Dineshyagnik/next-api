f0a1657296df9a130e1d26aeb49e41501ca54a94
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-08-28 15:50:28 +0530

    create api

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -8,6 +8,6 @@ export function GET(request,content){
     const userData=user.filter((item)=>item.id==content.params.id)
 
     return NextResponse.json(
-        userData.length==0?{result:'No Data Found',success:false}:{result:userData,success:true},
+        userData.length==0?{result:'No Data Found',success:false}:{result:userData[0],success:true},
         {status:200})
 }
---
+++ src/app/users/page.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+async function getUsers() {
+    let data = await fetch("http://localhost:3000/api/users");
+    data = await data.json();
+    return data;
+}
+
+export default async function Page() {
+
+    const users = await getUsers();
+    console.log(users);
+
+    return (
+        <div>
+            <h1>User List</h1>
+            {
+                users.map((user) => (
+                    // <h3>Name : {user.name}, Age: {user.age}, Email: {user.email}</h3>
+                    <div>
+
+                        <Link href={`users/${user.id}`}>{user.name}</Link>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
