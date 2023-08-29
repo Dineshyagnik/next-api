0782c607c282937063b08ed54379d0b0323df0df
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-08-29 15:44:40 +0530

    Make API with Post Method

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -6,3 +6,12 @@ export function GET(request){
     const data = user;
     return NextResponse.json(data,{status:200})
 }
+
+export async function POST(req){
+    let payload = await req.json();
+    console.log(payload.name);
+    if(!payload.name || !payload.age || !payload.email){
+        return NextResponse.json({result:"require field not found",success:false},{status:400})
+    }
+    return NextResponse.json({result:"new user created",success:true},{status:201})
+}
