e4c864136e497ac96d8864bb3bc8402ed6ed2dcb
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-09-03 14:50:34 +0530

    update

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,2 +1,2 @@
-const {username,password}=process.env;
-export const connectionStr = "mongodb+srv://"+username+":"+password+"@cluster0.n0lmgmo.mongodb.net/productDB?retryWrites=true&w=majority";
+const { username, password } = process.env;
+export const connectionStr = "mongodb+srv://" + username + ":" + password + "@cluster0.n0lmgmo.mongodb.net/productDB?retryWrites=true&w=majority";
