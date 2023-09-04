f0ef031d7cfc55296cac78ce7d3088065850c8df
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-09-04 15:51:59 +0530

    connect mongoDB

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,10 +1,17 @@
 import { connectionStr } from "@/lib/db";
+import { Product } from "@/lib/model/product";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 export async function GET() {
 
-    await mongoose.connect(connectionStr)
+    let data = [];
+    try {
+        await mongoose.connect(connectionStr);
+        const data = await Product.find();
+    } catch (error) {
+        data = { success: false }
+    }
 
-    return NextResponse.json({ result: true })
+    return NextResponse.json({ result: data, success: true })
 }
--- src/lib/db.js
+++ src/lib/db.js
@@ -1,2 +1,2 @@
 const { username, password } = process.env;
-export const connectionStr = "mongodb+srv://" + username + ":" + password + "@cluster0.n0lmgmo.mongodb.net/productDB?retryWrites=true&w=majority";
+export const connectionStr="mongodb+srv://"+username+":"+password+"@cluster0.n0lmgmo.mongodb.net/productDB?retryWrites=true&w=majority";
